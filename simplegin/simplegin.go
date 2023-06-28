139f58b4767837db9f139d837327b0d9df8ec96d
LongPeng <longpeng96@qq.com>
2023-06-28 18:14:30 +0800

    import new router

--- simplegin/simplegin.go
+++ simplegin/simplegin.go
@@ -1,33 +1,26 @@
 package simplegin
 
 import (
-	"fmt"
 	"net/http"
 )
 
-type HandlerFunc func(http.ResponseWriter, *http.Request)
+type HandlerFunc func(*Context)
 
 type Engine struct {
-	router map[string]HandlerFunc
+	router *router
 }
 
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	key := req.Method + "-" + req.URL.Path
-	if handler, ok := engine.router[key]; ok {
-		handler(w, req)
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
-	}
+	c := newContext(w, req)
+	engine.router.handle(c)
 }
 
 func New() *Engine {
-	return &Engine{router: make(map[string]HandlerFunc)}
+	return &Engine{router: newRouter()}
 }
 
-func (engine *Engine) addRoute(httpMethod string, pattern string, handler HandlerFunc) {
-	key := httpMethod + "-" + pattern
-	engine.router[key] = handler
+func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+	engine.addRoute(method, pattern, handler)
 }
 
 func (engine *Engine) Get(pattern string, handler HandlerFunc) {
