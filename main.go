12bff2e00979be8c93e03cc32dec6d89559c5bbe
LongPeng <longpeng96@qq.com>
2023-06-29 17:27:31 +0800

    using new version of simplegin with trie

--- main.go
+++ main.go
@@ -1,18 +1,40 @@
 package main
 
 import (
-	"SimpleGin/simplegin"
-	"fmt"
 	"net/http"
+	"simpleGin/simplegin"
 )
 
 func main() {
 	r := simplegin.New()
-	r.Get("/hello", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "hello there, it is version 2 of simplegin")
+	r.GET("/", func(c *simplegin.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello there!</h1>")
 	})
-	r.Get("/", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "this is index, it is version 2 of simplegin")
+
+	r.GET("/hello", func(c *simplegin.Context) {
+		// expect /hello?name=geektutu
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
 	})
+
+	r.GET("/hello/:name", func(c *simplegin.Context) {
+		// expect /hello/geektutu
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+	})
+
+	r.GET("/assets/*filepath", func(c *simplegin.Context) {
+		c.JSON(http.StatusOK, simplegin.H{"filepath": c.Param("filepath")})
+	})
+
 	r.Run(":9999")
 }
+
+//func main() {
+//	r := simplegin.New()
+//	r.GET("/hello", func(w http.ResponseWriter, req *http.Request) {
+//		fmt.Fprintf(w, "hello there, it is version 2 of simplegin")
+//	})
+//	r.GET("/", func(w http.ResponseWriter, req *http.Request) {
+//		fmt.Fprintf(w, "this is index, it is version 2 of simplegin")
+//	})
+//	r.Run(":9999")
+//}
